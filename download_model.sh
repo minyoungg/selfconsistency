c173e6a24e2aa375317310718bd0ff1d8b4079ee
Andrew Liu <ahliu@google.com>
2021-05-05 19:06:08 -0600

    update dead link to gdownload from drive

--- download_model.sh
+++ download_model.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
+
 echo "Downloading exif_final.zip"
 
-echo "Saving to ./ckpt/exif_final.zip"
-
-URL=http://people.eecs.berkeley.edu/~owens/consistency/exif_final.zip
-MODEL_FILE=exif_final.zip
-wget -N $URL -O $MODEL_FILE
+# Google Drive link to exif_final.zip
+gdown https://drive.google.com/uc?id=1X6b55rwZzU68Mz1m68WIX_G2idsEw3Qh
+echo "Unzipping to ./ckpt/exif_final.zip"
 
 mkdir -p ./ckpt/
 unzip exif_final.zip -d ./ckpt/
